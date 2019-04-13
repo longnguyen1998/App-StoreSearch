795c3b2a40dce48bba9b878c61e6a00fda1db913
zijievv <zijie_vv@icloud.com>
2019-04-13 16:18:14 +0300

    c32 Add: Creating the UI Fake searches data model "No results found" situation

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by zijie vv on 13/04/2019.
+//  Copyright © 2019 zijie vv. All rights reserved.
+//
+
+import Foundation
+
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
+
